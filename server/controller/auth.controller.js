7c54198c82f559785954fcee6120882208c00665
Anup Kumar Jana <janaanup7872@gmail.com>
2023-11-24 00:23:39 +0530

    created a custom middleware to handle possible errors

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body; // in this object we are getting the data
   const hashedPassword = bcrypt.hashSync(password, 10); //here we are encrypting the password
   const newUser = new User({ username, email, password: hashedPassword }); //we are storing the data in database
@@ -10,6 +11,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("User created Successfully!");
   } catch (err) {
-    res.status(500).json(err.message); //this is for if the username is not unique then its gonna throw error
+    next(error); //this is for--> if the username is not unique then its gonna throw error
   }
 };
--- server/index.js
+++ server/index.js
@@ -29,6 +29,18 @@ app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter)
 
 
+//middleware-- for error handling
+app.use((err, req, res, next)=>{    //next is for going to the next middleware
+    const statusCode= err.statusCode || 500  //here we're saying if there is not any statuscode from the err then send 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({      //its gonna throw details about the error
+      success: false,                          //if its successfull or not
+      statusCode,                               //whats the error status code
+      message,                                  //whats the error message we're getting
+    });
+})
+//--------------------------------
+
 const port = 8000;
 app.listen(port, () => {
   console.log("The server is running on 8000");
