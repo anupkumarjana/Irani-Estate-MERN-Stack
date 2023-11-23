a8a1f14daf198200f24ad1b5cd5cc783cb03720a
Anup Kumar Jana <janaanup7872@gmail.com>
2023-11-23 20:51:21 +0530

    created a test server route

---
+++ server/controller/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req, res) => {
+  res.json({ message: "Hi! this is Anup" });
+};
+
+
+//compare this code with server/routes/user.route.js
--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("MogoDB is connected");
-}) .catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Hi this is server</h1>`);
-});
+// app.get("/test", (req, res) => {
+//   res.send(`<h1>Hi this is server</h1>`);
+// });
+
+app.use("/server/user", userRouter);
 
 const port = 8000;
 app.listen(port, () => {
   console.log("The server is running on 8000");
 });
+
+
+
+
