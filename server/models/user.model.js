29a7c788d91a68e78d47f24d036dd2e2b1ae8cd4
Anup Kumar Jana <janaanup7872@gmail.com>
2023-11-23 16:23:25 +0530

    created user model for databse

---
+++ server/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true } //to record the login time we're storing this info
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
