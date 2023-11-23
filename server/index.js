e1da649ab65d36a3bd209b678d607fb6bb314dd9
Anup Kumar Jana <janaanup7872@gmail.com>
2023-11-23 15:26:40 +0530

    connected to database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
-
-app.get("/",(req, res)=>{
-    res.send(`<h1>Hi this is server</h1>`)
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MogoDB is connected");
+}) .catch((err)=>{
+    console.log(err);
 })
 
+app.get("/", (req, res) => {
+  res.send(`<h1>Hi this is server</h1>`);
+});
 
 const port = 8000;
 app.listen(port, () => {
