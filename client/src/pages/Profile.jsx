20e05d3ab405feea0ec30064738e88e5ffe79036
Anup Kumar Jana <janaanup7872@gmail.com>
2023-11-28 19:38:16 +0530

    we have to set the file, setFile asd null, otherwise its gonna throw error

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react"; //for updating profile image
 import { useSelector } from "react-redux";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(null);
+  const [filePercentage, setFilePercentage]= useState(0)
+  // console.log(file);
+  console.log((filePercentage));
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", (snpashot) => {
+      const progress = (snpashot.bytesTransferred / snpashot.totalBytes) * 100;
+      // console.log("Upload is" + progress + "% done");
+      setFilePercentage(Math.round(progress))
+    });
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       {/* <h1 className="text-3xl font-semibold text-center my-7">Profile</h1> */}
       <form className="flex flex-col my-7 gap-4">
+        <input
+          onChange={(event) => setFile(event.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w0-24 object-cover cursor-pointer self-center mt-2"
